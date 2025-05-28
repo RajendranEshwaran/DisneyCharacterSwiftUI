538ff3809c52d330a99820be346e740fa18cab5c
RajendranEshwaran <rajendranmca2010@gmail.com>
2025-05-28 14:07:44 -0400

    Initial Commit

---
+++ DemoTwoSwiftUI/DemoTwoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTwoSwiftUIApp.swift
+//  DemoTwoSwiftUI
+//
+//  Created by RajayGoms on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoTwoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
